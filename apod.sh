49ac7699a28511ccbd2b06877efe0b9763139a8c
Kimbsy <lordkimber@gmail.com>
2015-02-01 21:29:34 +0000

    much better idea, just run it every day :)

--- apod.sh
+++ apod.sh
@@ -1,3 +1,25 @@
-# grab all the pics from NASA's astonomy picture of the day site
+# grab the most recent image from NASA's astonomy picture of the day site
 
-wget -r -P ~/Pictures/NASA -A jpeg,jpg,bmp,gif,png http://apod.nasa.gov
+# get the html content of the front page
+CONTENT=$(wget apod.nasa.gov/apod/astropix.html -q -O -)
+
+echo $CONTENT
+
+# find the src of the image (there should only be one)
+REGEX="SRC=\"image\/[0-9]+\/.+.jpg"
+
+if [[ $CONTENT =~ $REGEX ]]; then
+  echo "match found"
+  echo "matching substring: ${BASH_REMATCH[0]}"
+
+  # strip the extra characters from the source
+  SOURCE=$(echo ${BASH_REMATCH[0]/SRC=/} | tr -d \")
+
+  echo $SOURCE
+
+  # download the image with wget
+  wget -P ~/Pictures/NASA http://apod.nasa.gov/$SOURCE
+    
+else
+    echo "no match found"
+fi
