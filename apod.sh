df818cb39a138e9ee2402a96a69ef81d6a08b3db
Kimbsy <lordkimber@gmail.com>
2015-02-01 20:14:37 +0000

    apod wget script

--- apod.sh
+++ apod.sh
@@ -1,3 +1,3 @@
 # grab all the pics from NASA's astonomy picture of the day site
 
-wget -r -P ~/Pictures -A jpeg,jpg,bmp,gif,png http://apod.nasa.gov
+wget -r -P ~/Pictures/NASA -A jpeg,jpg,bmp,gif,png http://apod.nasa.gov
